0ba524912390ea8452eec0095d9dc5a9a32fcf20
weatherman03 <aaron.m.weatherly@gmail.com>
2017-11-08 00:20:40 -0600

    Mapping of track data complete

--- Android/MainActivity.java
+++ Android/MainActivity.java
@@ -38,6 +38,7 @@ import com.hoho.android.usbserial.util.SerialInputOutputManager;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -50,19 +51,22 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     private Camera mCamera;
     private TextureView mTextureView;
     private SurfaceView mSurfaceView;
+    private SurfaceView mSurfaceView2;
     private SurfaceHolder mSurfaceHolder;
+    private SurfaceHolder mSurfaceHolder2;
     private Bitmap bmp = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
     private Canvas canvas = new Canvas(bmp);
+    private Bitmap bmp2 = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
     private Paint paint1 = new Paint();
     private TextView myTextView;
     private TextView myTextView2;
-    private TextView myTextView3;
     private SeekBar threshold;
-    private SeekBar threshold2;
     private Button button;
-    int thresh = 100;
-    int thresh2 = 240;
+    int thresh = 120;
+    //int thresh2 = 190;
     int go = 0;
+    int xTrack = 0, yTrack = 0;
+    float xCar = 0, yCar = 0, theta = 0, lapTime = 0;
 
     private UsbManager manager;
     private UsbSerialPort sPort;
@@ -75,17 +79,15 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // keeps the screen from turning off
 
         threshold = (SeekBar) findViewById(R.id.seek1);
-        threshold2 = (SeekBar) findViewById(R.id.seek2);
         myTextView = (TextView) findViewById(R.id.textView01);
         myTextView2 = (TextView) findViewById(R.id.textView02);
-        myTextView3 = (TextView) findViewById(R.id.textView03);
         button = (Button) findViewById(R.id.button1);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { // change state variable
                 go = go + 1;
-                if (go > 1) {
+                if (go > 2) {
                     go = 0;
                     // send 641 to the PIC to stop motors (outside of expected range for COM)
                     int temp = 641;
@@ -105,6 +107,9 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
             mSurfaceHolder = mSurfaceView.getHolder();
+
+            mSurfaceView2 = (SurfaceView) findViewById(R.id.surfaceview2);
+            mSurfaceHolder2 = mSurfaceView2.getHolder();
 
             mTextureView = (TextureView) findViewById(R.id.textureview);
             mTextureView.setSurfaceTextureListener(this);
@@ -153,21 +158,6 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 thresh = progress;
                 myTextView.setText("Blue Greater Than: "+thresh);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-        threshold2.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                thresh2 = progress;
-                myTextView2.setText("Start Y: "+thresh2);
             }
 
             @Override
@@ -278,14 +268,25 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     }
 
     private void updateReceivedData(byte[] data) {
-        //do something with received data
 
-        //for displaying:
+        // store/display received data as xCar,yCar,theta,lapTime variables
         if (go ==1) {
             String rxString = null;
             try {
                 rxString = new String(data, "UTF-8"); // put the data you got into a string
-                myTextView3.setText(rxString);
+                //sscanf(rxString,"%f %f %f %f",&xCar,&yCar,&theta,&lapTime); // need java equivalent of this
+                Scanner scan = new Scanner(rxString);
+                float buffer[] = new float[4];
+                int i = 0;
+                while (scan.hasNextFloat()) {
+                    buffer[i] = scan.nextFloat();
+                    i++;
+                }
+                xCar = buffer[0];
+                yCar = buffer[1];
+                theta = buffer[2];
+                lapTime = buffer[3];
+                myTextView2.setText(rxString);
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -294,50 +295,80 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
 
     // the important function
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        int M, sum, COM, COMavg, count;
         if (go == 1) {
-            int M = 0;
-            int sum = 0;
-            int COM;
+            M = 0;
+            sum = 0;
+            COMavg = 0;
+            count = 0;
             // every time there is a new Camera preview frame
             mTextureView.getBitmap(bmp);
 
             final Canvas c = mSurfaceHolder.lockCanvas();
+            final Canvas c2 = mSurfaceHolder2.lockCanvas();
             if (c != null) {
                 int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
+                for (int startY = 180; startY <= 300; startY = startY + 20) { // which row in the bitmap to analyze to read
+                    bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+                    // in the row, see if the pixel is blue
+                    for (int i = 0; i < bmp.getWidth(); i++) {
+                        if (blue(pixels[i]) > thresh) {
+                            pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                            // COM pixel calculation
+                            M = M + 1;
+                            sum = sum + i;
+                        }
+                    }
+                    // update the row
+                    bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
 
-                int startY = thresh2; // which row in the bitmap to analyze to read
-                bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
-                // in the row, see if the pixel is grey or brown
-                for (int i = 0; i < bmp.getWidth(); i++) {
-                    if (blue(pixels[i]) > thresh) {
-                        pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
-                        // COM pixel calculation
-                        M = M + 1;
-                        sum = sum + i;
+                    // calculate COM for each row
+                    if (M == 0) {
+                        COM = 0;
+                    } else {
+                        COM = (sum / M) + 1;
+                        COMavg += COM;
+                        count++;
                     }
                 }
-                // update the row
-                bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
-
-                // draw a circle at COM
-                if (M == 0) {
-                    COM = 0;
+                // average the COMs
+                if (count == 0) {
+                    COMavg = 0;
                 } else {
-                    COM = (sum / M) + 1;
+                    COMavg = COMavg / count;
+                    // calculate a point on the track based on COMavg, xCar, yCar, theta
+                    float l = (float) (0.025*COMavg); // distance from left of frame to the COM (in cm)
+                    float d = (float) (Math.sqrt(Math.pow(18.2,2) - Math.pow(l,2))); // distance from the car to the COM (in cm)
+                    float theta2 = (float) (Math.atan2(8-l,16.35)); // angle from the car to the COM along a line 16.35 cm from car's position (in radians)
+                    xTrack = (int) (xCar*100 + d*Math.cos(theta + theta2));
+                    yTrack = 500 - (int) (yCar*100 + d*Math.sin(theta + theta2));
+                    // add that point to the track bitmap
+                    if (c2 != null) {
+                        try {
+                            bmp2.setPixel(xTrack, yTrack, 0xffffffff);
+                        } catch (Exception e) {}
+                    }
                 }
-                canvas.drawCircle(COM, startY, 5, paint1); // x position, y position, diameter, color
-
+                // draw circle at COM
+                canvas.drawCircle(COMavg, 240, 5, paint1);
                 // write the COM as text
-                canvas.drawText("COM = " + COM, 10, 30, paint1);
-
+                canvas.drawText("COM = " + COMavg, 10, 30, paint1);
                 //send COM to PIC
-                String sendString = String.valueOf(COM) + '\n';
+                String sendString = String.valueOf(COMavg) + '\n';
                 try {
                     sPort.write(sendString.getBytes(), 10); // 10 is the timeout
                 } catch (IOException e) {}
             }
             c.drawBitmap(bmp, 0, 0, null);
             mSurfaceHolder.unlockCanvasAndPost(c);
+            c2.drawBitmap(bmp2, 0, 0, null);
+            mSurfaceHolder2.unlockCanvasAndPost(c2);
+
+            // add condition for first lap complete (based on position and lapTime), then calculate racing line and change to go ==  2
+
         }
+
+        // add if (go == 2) {} statement for once the first lap is complete, then use the racing line for feedback control
+
     }
 }
